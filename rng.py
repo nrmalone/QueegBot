e3184218c84b471f0022ec9bae229eaa0819c2e9
reecemalone <reecemalone03@gmail.com>
2022-04-21 13:04:32 -0700

    Fixed roll functionality, corrected default values

--- rng.py
+++ rng.py
@@ -18,15 +18,21 @@ class rng(commands.Cog):
     #roll <# of dice> <type of die>
     @commands.command()
     async def roll(self,ctx,diceQuantity=None,diceType=None):
+        await ctx.message.delete()
         roll = 0
         if 'd' in ctx.message.content:
             roll_text = (ctx.message.content[6:]).split('d')
             diceQuantity = str(roll_text[0].strip())
             diceType = str(roll_text[1].strip())
-        if str.isnumeric(diceQuantity):
-            for die in range(1, int(diceQuantity)):
-                roll += random.randint(1,int(diceType))
-        await ctx.send(":game_die: " + (diceQuantity + "d" + diceType + " :arrow_right: " + str(roll)), delete_after=30)
+        if not str.isnumeric(diceQuantity):
+            diceQuantity = '1'
+        if not str.isnumeric(diceType):
+            await ctx.send("Learn to TYPE properly, MAN!!!", delete_after=10)
+            return
+        for die in range(int(diceQuantity)):
+            die_roll = random.randint(1,int(diceType))
+            roll += die_roll
+        await ctx.send(":game_die: " + (diceQuantity + "d" + diceType + " :arrow_right: " + str(roll)), delete_after=60)
 
 def setup(client):
     client.add_cog(rng(client))
