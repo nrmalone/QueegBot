57e6995f74cc777c003e45ef86961258d54644c9
reecemalone <reecemalone03@gmail.com>
2022-04-21 01:27:31 -0700

    +chat.py, *claymore, *update, +XdY for roll

--- cards.py
+++ cards.py
@@ -1,31 +1,24 @@
 import random
 import discord
 from discord.ext import commands
+from asyncio import TimeoutError as TE
 
 class cards(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+
     @commands.command()
     async def cards(self, ctx):
         """
-        Provides the user (via a Discord message) of which card games
-        are available in this module (MANUALLY EDITED)
+        Provides the user (via a Discord message) of a list of card games
+        which are available in this module (MANUALLY EDITED)
         """
         games = ["Poker", "Blackjack", "Speed"]
         msg = "Available games:\n"
         for i, game in enumerate(games):
             msg += f"\t{i+1}.\t{game}\n"
         await ctx.send(msg, delete_after=60)
-    
-
-    @commands.command()
-    async def claymore(self, ctx):
-        """
-        Places a friendly M18 A1 APERSMINE for
-        users using the text channel
-        """
-        await ctx.send(file=discord.File(fp='.\claymore.png'))
 
 def setup(client):
     client.add_cog(cards(client))
