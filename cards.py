d105f170ef7b2faf4bc78c3eab1a265566152c1a
reecemalone <reecemalone03@gmail.com>
2022-04-20 19:39:35 -0700

    Added customize.py, gen organization

--- cards.py
+++ cards.py
@@ -15,22 +15,9 @@ class cards(commands.Cog):
         games = ["Poker", "Blackjack", "Speed"]
         msg = "Available games:\n"
         for i, game in enumerate(games):
-            msg += f"\t{i+1}. {game}\n"
+            msg += f"\t{i+1}.\t{game}\n"
         await ctx.send(msg, delete_after=60)
-    
-    # TODO: Move this command to a newly created "customize.py"
-    @commands.command()
-    
-    async def update(self, ctx, game_input):
-        """
-        Allows the user to update the bot user's
-        "Playing {game}" status text
-        """
-        if len(game_input) > 40:
-            ctx.send(str("CALM THE FUCK DOWN, MAAAAAN!!!"))
-            game_input = f"LoL with {str(ctx.author)[:-5]}"
-        game = discord.Game(str(game_input))
-        await self.client.change_presence(activity=game)
+
 
 def setup(client):
     client.add_cog(cards(client))
---
+++ customize.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+
+class customize(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def update(self, ctx, game_input):
+        """
+        Allows the user to update the bot user's
+        "Playing {game}" status text
+        """
+        if len(game_input) > 40:
+            ctx.send(str("CALM THE FUCK DOWN, MAAAAAN!!!"))
+            game_input = f"LoL with {str(ctx.author)[:-5]}"
+        game = discord.Game(str(game_input))
+        await self.client.change_presence(activity=game)
+
+
+def setup(client):
+    client.add_cog(customize(client))
