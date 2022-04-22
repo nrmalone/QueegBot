46758b1fec8a70ab195eda72e38c5ab88784ebdb
reecemalone <reecemalone03@gmail.com>
2022-04-21 23:58:39 -0700

    Added rng command

--- rng.py
+++ rng.py
@@ -13,6 +13,7 @@ class rng(commands.Cog):
         Allows users to flip a coin with command "coin"
         Sends heads or tails message according to result
         """
+        await ctx.message.delete
         coin = random.randint(0,1)
         if coin == 0:
             await ctx.send(":coin: heads", delete_after=30)
@@ -24,7 +25,7 @@ class rng(commands.Cog):
     async def roll(self,ctx,diceQuantity=None,diceType=None):
         """
         Allows users to roll 1 or more dice of a specified length
-        No provided dice count defaults to 1, command specified below:
+        No provided dice count defaults to 1
         
         "roll <# of dice> <type of die>" 
         or 
@@ -45,6 +46,29 @@ class rng(commands.Cog):
             die_roll = random.randint(1,int(diceType))
             roll += die_roll
         await ctx.send(":game_die: " + (diceQuantity + "d" + diceType + " :arrow_right: " + str(roll)), delete_after=60)
+
+    @commands.command()
+    async def rng(self, ctx, *args):
+        """
+        Generates & sends a random number specified
+        by either 1 number, or by a range of 2 given numbers
+
+        "rng <max #>"
+        or
+        "rng <min #> <max #>"
+        """
+        await ctx.message.delete()
+        try:
+            start = 1
+            stop = int(args[0])
+            if len(args) > 1:
+                start = int(args[0])
+                stop = int(args[1])
+            num = random.randint(start, stop)
+            await ctx.send(":1234:  " + str(num) + "  :1234:", delete_after=60)
+        except:
+            await ctx.send("Learn to TYPE properly, MAN!!!", delete_after=10)
+
 
 def setup(client):
     client.add_cog(rng(client))
